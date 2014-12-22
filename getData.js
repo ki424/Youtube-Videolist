331dc089243a884682fbd1f58ec957c073ffde36
Yves <yv3s1157@outlook.com>
2014-12-22 22:29:58 +0100

    Second commit!

--- getData.js
+++ getData.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
     $.getJSON("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=UUQvTDmHza8erxZqDkjQ4bQQ&key=AIzaSyDm4ef2FPkjWE2iFFWS7bNFNk6UWd4pNFM", function(data) {
         for (var i = 0; i < data.items.length; i++) {
-            $(".video-list").append('<li class="video-item"><div class="video-img"><img class="logo" src="' + data.items[i].snippet.thumbnails.default.url + '"></div><span class="descr">' + data.items[i].snippet.title + '</span></li>');
+            $(".video-list").append('<li class="video-item"><a href="https://www.youtube.com/watch?v=' + data.items[i].snippet.resourceId.videoId + '"><div class="video-img"><img class="logo" src="' + data.items[i].snippet.thumbnails.default.url + '"></div><span class="descr">' + data.items[i].snippet.title + '</span></a></li>');
         }
 
         $("body").delegate('.video-item', 'click', function() {
@@ -21,31 +21,30 @@ $(document).ready(function() {
 
         $("#add").click(function() {
 
-  
+
             $(".video-list").empty();
             channel = $('#channel').val();
             //https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=rocketbeanstv&key=AIzaSyDm4ef2FPkjWE2iFFWS7bNFNk6UWd4pNFM
             $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=" + channel + "&key=AIzaSyDm4ef2FPkjWE2iFFWS7bNFNk6UWd4pNFM", function(data) {
-                
-					if(data.items.length <= 0){
-						alert("Error!");
-						for(var i = 0; i < localStorage.length; i++) {
-							alert(localStorage.key(i));
-							alert(localStorage[localStorage.key(i)]);
-						}
-					} 
-					else {
-						
-						id = data.items[0].contentDetails.relatedPlaylists.uploads;	
-						localStorage[channel] = id;
-							
-						$.getJSON("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=" + id + "&key=AIzaSyDm4ef2FPkjWE2iFFWS7bNFNk6UWd4pNFM", function(data) {
-							for (var i = 0; i < data.items.length; i++) {
 
-								$(".video-list").append('<li class="video-item"><div class="video-img"><img class="logo" src="' + data.items[i].snippet.thumbnails.default.url + '"></div><span class="descr">' + data.items[i].snippet.title + '</span></li>');
-							}
-						});
-					}
+                if (data.items.length <= 0) {
+                    alert("Error!");
+                    for (var i = 0; i < localStorage.length; i++) {
+                        alert(localStorage.key(i));
+                        alert(localStorage[localStorage.key(i)]);
+                    }
+                } else {
+
+                    id = data.items[0].contentDetails.relatedPlaylists.uploads;
+                    localStorage[channel] = id;
+
+                    $.getJSON("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=" + id + "&key=AIzaSyDm4ef2FPkjWE2iFFWS7bNFNk6UWd4pNFM", function(data) {
+                        for (var i = 0; i < data.items.length; i++) {
+
+                            $(".video-list").append('<li class="video-item"><div class="video-img"><img class="logo" src="' + data.items[i].snippet.thumbnails.default.url + '"></div><span class="descr">' + data.items[i].snippet.title + '</span></li>');
+                        }
+                    });
+                }
             });
         });
 
@@ -55,15 +54,15 @@ $(document).ready(function() {
         });
 
         $('.add-menu').click(function() {
-       		$('#adding').slideToggle();
-			$('#deleting').slideUp();
+            $('#adding').slideToggle();
+            $('#deleting').slideUp();
         });
-		
-		 $('.delete-menu').click(function() {
-       		$('#deleting').slideToggle();
-			$('#adding').slideUp();
+
+        $('.delete-menu').click(function() {
+            $('#deleting').slideToggle();
+            $('#adding').slideUp();
         });
-		
-		
+
+
     });
 });
